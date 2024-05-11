c5a717a7dc284012b35068d8cdb8622f6a62f861
Xingwei <912254883@qq.com>
2024-05-11 12:38:57 +0800

    feat: 猜你喜欢类型定义和列表渲染

--- src/components/XtxGuess.vue
+++ src/components/XtxGuess.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
-//
+import { getHomeGoodsGuessLikeAPI } from '@/services/home';
+import type { GuessItem } from '@/types/home';
+import { onMounted, ref } from 'vue';
+
+const guessList = ref<GuessItem[]>([])
+// 获取猜你喜欢数据
+const getHomeGoodsGuessLikeData = async() => {
+  const res = await getHomeGoodsGuessLikeAPI()
+  guessList.value = res.result.items
+}
+// 组价挂载完毕
+onMounted(() => {
+  getHomeGoodsGuessLikeData()
+})
 </script>
 
 <template>
@@ -10,19 +23,19 @@
   <view class="guess">
     <navigator
       class="guess-item"
-      v-for="item in 10"
-      :key="item"
-      :url="`/pages/goods/goods?id=4007498`"
+      v-for="item in guessList"
+      :key="item.id"
+      :url="`/pages/goods/goods`"
     >
       <image
         class="image"
         mode="aspectFill"
-        src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_big_1.jpg"
-      ></image>
-      <view class="name"> 德国THORE男表 超薄手表男士休闲简约夜光石英防水直径40毫米 </view>
+        :src="item.picture"
+        ></image>
+      <view class="name">{{item.name}}</view>
       <view class="price">
         <text class="small">¥</text>
-        <text>899.00</text>
+        <text>{{item.price}}</text>
       </view>
     </navigator>
   </view>
