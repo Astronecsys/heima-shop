3bed8eec7c14e453afc6687adada15b6c5c7b76c
Xingwei <912254883@qq.com>
2024-05-11 13:44:29 +0800

    feat: 首页骨架屏

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,39 +1,43 @@
 <script setup lang="ts">
-import { getHomeBannerAPI, getHomeCategoryAPI,getHomeHotAPI } from '@/services/home';
-import { onLoad } from '@dcloudio/uni-app';
-import { ref } from 'vue';
-import type { BannerItem, CategoryItem,HotItem } from '@/types/home';
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 import CustomNavbar from './componets/CustomNavbar.vue'
 import CategoryPanel from './componets/CategoryPanel.vue'
 import HotPanel from './componets/HotPanel.vue'
-import type { XtxGuessInstance } from '@/types/components';
+import type { XtxGuessInstance } from '@/types/components'
+import PageSkeleton from './componets/PageSkeleton.vue'
 
 //获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
-const getHomeBannerData = async() => {
+const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   bannerList.value = res.result
 }
 
 // 获取前台分类数据
 const categoryList = ref<CategoryItem[]>([])
-const getHomeCategoryData = async() => {
+const getHomeCategoryData = async () => {
   const res = await getHomeCategoryAPI()
   categoryList.value = res.result
 }
 
 // 获取热门推荐数据
 const hotList = ref<HotItem[]>([])
-const getHomeHotData = async() => {
+const getHomeHotData = async () => {
   const res = await getHomeHotAPI()
   hotList.value = res.result
 }
 
+// 是否加载中
+const isLoading = ref(false)
+
 // 页面加载
-onLoad(() => {
-  getHomeBannerData()
-  getHomeCategoryData()
-  getHomeHotData()
+onLoad(async () => {
+  isLoading.value = true
+  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()])
+  isLoading.value = false
 })
 
 // 获取猜你喜欢组件实例
@@ -45,8 +49,9 @@ const onScrolltolower = () => {
 
 // 自定义下拉刷新被触发
 const isTrigger = ref(false)
-const onRefresherrefresh = async() => {
+const onRefresherrefresh = async () => {
   isTrigger.value = true
+  isLoading.value = true
   // await getHomeBannerData()
   // await getHomeCategoryData()
   // await getHomeHotData()
@@ -58,28 +63,34 @@ const onRefresherrefresh = async() => {
     guessRef.value?.getMore(),
   ])
   isTrigger.value = false
+  isLoading.value = false
 }
-
 </script>
 
 <template>
   <!-- 自定义导航栏 -->
   <CustomNavbar />
   <!-- 滚动容器 -->
-  <scroll-view 
-  refresher-enabled 
-  @refresherrefresh="onRefresherrefresh" 
-  :refresher-triggered="isTrigger"
-  @scrolltolower="onScrolltolower" 
-  class="scroll-view" scroll-y>
-    <!-- 自定义轮播图 -->
-    <XtxSwiper :list="bannerList" />
-    <!-- 分类面板 -->
-    <CategoryPanel :list="categoryList"/>
-    <!-- 热门推荐 -->
-    <HotPanel :list="hotList"/>
-    <!-- 猜你喜欢 -->
-    <XtxGuess ref="guessRef" />
+  <scroll-view
+    refresher-enabled
+    @refresherrefresh="onRefresherrefresh"
+    :refresher-triggered="isTrigger"
+    @scrolltolower="onScrolltolower"
+    class="scroll-view"
+    scroll-y
+  >
+    <!-- 骨架屏 -->
+    <PageSkeleton v-if="isLoading" />
+    <template v-else>
+      <!-- 自定义轮播图 -->
+      <XtxSwiper :list="bannerList" />
+      <!-- 分类面板 -->
+      <CategoryPanel :list="categoryList" />
+      <!-- 热门推荐 -->
+      <HotPanel :list="hotList" />
+      <!-- 猜你喜欢 -->
+      <XtxGuess ref="guessRef" />
+    </template>
   </scroll-view>
 </template>
 
@@ -90,7 +101,7 @@ page {
   display: flex;
   flex-direction: column;
 }
-.scroll-view{
+.scroll-view {
   flex: 1;
 }
 </style>
