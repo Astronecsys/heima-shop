2b79d3e2a7330088112662b615bed4643d14bab0
Xingwei <912254883@qq.com>
2024-05-11 12:52:17 +0800

    fix: 修正类型定义位置

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -1,6 +1,6 @@
 // src/types/components.d.ts
-import XtxSwiper from './XtxSwiper.vue'
-import XtxGuess from './XtxGuess.vue'
+import XtxSwiper from '@components/XtxSwiper.vue'
+import XtxGuess from '@components/XtxGuess.vue'
 declare module 'vue' {
   export interface GlobalComponents {
     XtxSwiper: typeof XtxSwiper
