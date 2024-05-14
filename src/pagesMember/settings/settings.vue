cf83d54b15065c95bc733da9fb40260dda503369
Xingwei <912254883@qq.com>
2024-05-14 20:10:26 +0800

    feat: 个人信息-静态结构

--- src/pagesMember/settings/settings.vue
+++ src/pagesMember/settings/settings.vue
@@ -22,7 +22,7 @@ const onLogout = () => {
 <template>
   <view class="viewport">
     <!-- 列表1 -->
-    <view class="list" v-if="true">
+    <view class="list" v-if="memberStore.profile">
       <navigator url="/pagesMember/address/address" hover-class="none" class="item arrow">
         我的收货地址
       </navigator>
@@ -38,7 +38,7 @@ const onLogout = () => {
       <navigator hover-class="none" class="item arrow" url=" ">关于小兔鲜儿</navigator>
     </view>
     <!-- 操作按钮 -->
-    <view class="action">
+    <view class="action" v-if="memberStore.profile">
       <view @tap="onLogout" class="button">退出登录</view>
     </view>
   </view>
